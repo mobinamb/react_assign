341de1da02077e3bc4de764585646c9408fbd105
Mobina <mobina.mb96@gmail.com>
2023-11-20 15:37:38 -0800

    introducing props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
 import BookList from './BookList';
 
-
 function App() {
+  // Define the list of books
+  const books = [
+    { title: 'Squire', author: 'Nadia Shammas' },
+    { title: 'Sisters of the Snake', author: 'Sasha Nanua' },
+    { title: 'Beyond the End of the World', author: 'Amie Kaufman' }
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <BookList />
+        {/* Other content */}
+        <BookList books={books} />
       </header>
     </div>
   );
--- src/BookList.js
+++ src/BookList.js
@@ -1,18 +1,11 @@
 import React from 'react';
 
-const BookList = () => {
-  // Define an array of books
-  const books = [
-    { title: 'Squire', author: 'Nadia Shammas' },
-    { title: 'Sisters of the Snake', author: 'Sasha Nanua' },
-    { title: 'Beyond the End of the World', author: 'Amie Kaufman' }
-  ];
-
+const BookList = (props) => {
   return (
     <div>
       <h3>Book List</h3>
       <ul>
-        {books.map((book, index) => (
+        {props.books.map((book, index) => (
           <li key={index}>{book.title} by {book.author}</li>
         ))}
       </ul>
