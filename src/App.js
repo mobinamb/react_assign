a22a1ba270131ddcceaba6ffc1cf254d37ad495c
Mobina <mobina.mb96@gmail.com>
2023-11-20 18:56:25 -0800

    styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import BookList from './BookList';
 import AddBookForm from './AddBookForm'; // You will create this component in the next step
-
+import './App.css';
 
 const apiBaseUrl = 'https://654166c1f0b8287df1fe51c2.mockapi.io/books'
 function App() {
